94f6fa6c3211084d514f883936f2258a79928d4e
omarali18 <omarali1814000@gmail.com>
2022-01-27 22:27:46 +0600

    experiences added

--- index.js
+++ index.js
@@ -14,7 +14,147 @@ app.use(express.json())
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.av6mz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
-console.log(uri);
+async function run() {
+    try {
+        await client.connect()
+        console.log("Database connected is");
+        const database = client.db("internTourist");
+        const experiencesCollection = database.collection("experiences");
+        // const usersCollection = database.collection("users")
+        // const doctorCollection = database.collection("doctors")
+
+        // get all experiences
+        app.get("/experiences", async (req, res) => {
+            const cursur = experiencesCollection.find({});
+            const experiences = await cursur.toArray();
+            res.json(experiences)
+        })
+
+        // app.get("/appointments", async (req, res) => {
+        //     const email = req.query?.email;
+        //     const date = req.query?.date;
+        //     const query = { email: email, date: date }
+        //     const cursor = appointmentCollection.find(query)
+        //     const appointment = await cursor.toArray()
+        //     res.send(appointment)
+        // });
+
+        // app.post("/appointments", async (req, res) => {
+        //     const appointment = req.body;
+        //     console.log(appointment);
+        //     const result = await appointmentCollection.insertOne(appointment)
+        //     res.json(result)
+        // });
+
+        // app.get("/appointments/:id", async (req, res) => {
+        //     const id = req.params.id;
+        //     const query = { _id: ObjectId(id) };
+        //     const result = await appointmentCollection.findOne(query);
+        //     res.json(result)
+        // });
+
+        // app.put("/appointments/:id", async (req, res) => {
+        //     const id = req.params.id
+        //     const payment = req.body
+        //     const query = { _id: ObjectId(id) }
+        //     const updateDoc = { $set: { payment: payment } }
+        //     const result = await appointmentCollection.updateOne(query, updateDoc)
+        //     res.json(result)
+        // })
+
+        //save doctor
+
+        // app.get("/doctors", async (req, res) => {
+        //     const cursur = doctorCollection.find({});
+        //     const doctors = await cursur.toArray();
+        //     res.json(doctors)
+        // })
+
+        // app.post("/doctors", async (req, res) => {
+        //     const name = req.body.name;
+        //     const email = req.body.email;
+        //     const pic = req.files.image;
+        //     const picData = pic.data;
+        //     const encodedPic = picData.toString("base64");
+        //     const imageBuffer = Buffer.from(encodedPic, "base64")
+        //     const doctor = {
+        //         name,
+        //         email,
+        //         image: imageBuffer
+        //     }
+        //     console.log(doctor);
+        //     const result = await doctorCollection.insertOne(doctor)
+        //     res.json(result)
+
+        // })
+
+        // save user informaton
+        // app.get("/users/:email", async (req, res) => {
+        //     const email = req.params.email
+        //     const filter = { email: email }
+        //     const user = await usersCollection.findOne(filter)
+        //     let isAdmin = false;
+        //     if (user?.role === 'Admin') {
+        //         isAdmin = true
+        //     }
+        //     res.json(isAdmin)
+        // })
+
+        // app.post("/users", async (req, res) => {
+        //     const user = req.body;
+        //     const result = await usersCollection.insertOne(user)
+        //     res.json(result)
+        // });
+
+        // app.put("/users", async (req, res) => {
+        //     const user = req.body;
+        //     const filter = { email: user.email };
+        //     const options = { upsert: true };
+        //     const updateDoc = { $set: user }
+        //     const result = await usersCollection.updateOne(filter, updateDoc, options)
+        //     res.json(result)
+        // });
+
+        // app.put("/users/admin", verifyToken, async (req, res) => {
+        //     const user = req.body;
+        //     const requester = req.decodedEmail;
+        //     if (requester) {
+        //         const requesterAccount = await usersCollection.findOne({ email: requester })
+        //         if (requesterAccount.role === "Admin") {
+        //             const filter = { email: user.email }
+        //             const updateDoc = { $set: { role: "Admin" } }
+        //             const result = await usersCollection.updateOne(filter, updateDoc);
+        //             res.json(result)
+        //         }
+        //     }
+        //     else {
+        //         res.status(403).json({ message: "You do not access to make admin." })
+        //     }
+
+        // })
+
+        // app.post("/create-payment-intent", async (req, res) => {
+        //     const paymentInfo = req.body
+        //     const amount = paymentInfo.price * 100;
+        //     const paymentIntent = await stripe.paymentIntents.create({
+        //         currency: "USD",
+        //         amount: amount,
+        //         automatic_payment_methods: {
+        //             enabled: true,
+        //         },
+        //     })
+        //     res.send({
+        //         clientSecret: paymentIntent.client_secret,
+        //     });
+        // })
+    }
+    finally {
+        // await client.close();
+
+    }
+}
+
+run().catch(console.dir)
 
 app.get('/', (req, res) => {
   res.send('Hello World!')

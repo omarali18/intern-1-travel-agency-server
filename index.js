016dcfc747c936957341bfd215df5db9d9c4cf80
omarali18 <omarali1814000@gmail.com>
2022-01-27 22:02:56 +0600

    database cunnect

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
 const express = require('express')
+const { MongoClient } = require('mongodb');
+const ObjectId = require("mongodb").ObjectId
+require('dotenv').config()
+const cors = require("cors")
 const app = express()
-const port =  5000
+
+
+const port = process.env.PORT || 5000
+
+app.use(cors());
+app.use(express.json())
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.av6mz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+console.log(uri);
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
